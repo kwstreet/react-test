0239630469f81d9d42486e4809407bb053940146
kwstreet <mm+git@blat.ca>
2022-05-23 20:38:48 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <p>This was a paragraph</p>
+    </div>
+  );
+}
